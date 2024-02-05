4d61570eaa04735a2a9e5d85c7101c6f45f3442a
Berkay Dinç <berkayydinc@gmail.com>
2024-02-05 16:28:06 +0300

    implement reclamation (with data race)

--- src/stack.rs
+++ src/stack.rs
@@ -1,4 +1,4 @@
-use std::sync::atomic::{Ordering, AtomicPtr};
+use std::sync::atomic::{Ordering, AtomicPtr, AtomicUsize};
 use std::ptr;
 
 struct Node<T> {
@@ -7,14 +7,26 @@ struct Node<T> {
 }
 
 pub struct Stack<T> {
+    /// The pointer pointer to the top of the stack.
     top: AtomicPtr<Node<T>>,
+
+    /// The number of threads currently popping some data.
+    pops: AtomicUsize,
+
+    /// The list of nodes to be deleted.
+    garbage: AtomicPtr<Node<T>>,
 }
 
 impl<T> Stack<T> {
     pub fn new() -> Self {
-        Self { top: AtomicPtr::new(ptr::null_mut()) }
+        Self {
+            top: AtomicPtr::new(ptr::null_mut()),
+            pops: AtomicUsize::new(0),
+            garbage: AtomicPtr::new(ptr::null_mut()),
+        }
     }
 
+    /// Push data into stack, this allocates a new node for the given data.
     pub fn push(&self, value: T) {
         let next = self.top.load(Ordering::Relaxed);
         let node = Box::into_raw(Box::new(Node { value, next }));
@@ -29,32 +41,89 @@ impl<T> Stack<T> {
         }
     }
 
+    /// Pop data from the stack.
     pub fn pop(&self) -> Option<T> {
+        self.pops.fetch_add(1, Ordering::SeqCst);
+
+        // Load the top node so that we can CAS.
         let mut top = self.top.load(Ordering::Relaxed);
 
         loop {
+            // Return None if the top pointer is null.
             if top.is_null() { return None }
 
+            // Read the next node after the current one.
+            //
             // SAFETY: None of the popping threads will ever mutate a
             // *non-exclusive* node through a pointer, so reading is safe.
             let next = unsafe { (*top).next };
 
+            // If the top pointer is still pointing to the value we've read at
+            // the beginning, swap it with the next node, if not try again with
+            // the new node.
             match self.top.compare_exchange_weak(top, next, Ordering::Relaxed, Ordering::Relaxed) {
                 Ok(_) => break,
                 Err(node) => top = node,
             }
         }
 
+        // Read the value of the node we've just popped.
+        //
         // SAFETY: The CAS loop has succeeded, meaning, the current thread is
         // the only one that popped the top node, and responsible for returning
         // the value of the top node to the caller.
-        //
-        // However, there may still be more than one reader accessing the top
-        // node through pointers, so it is *not* safe to deallocate the top node,
-        // and that is a reclamation problem. So we will just leak.
         let node = unsafe { ptr::read(top) };
 
+        // Reclaim the nodes if we can.
+        unsafe { self.reclaim(top) };
+
         Some(node.value)
+    }
+
+    unsafe fn reclaim(&self, node: *mut Node<T>) {
+        let pops = self.pops.load(Ordering::SeqCst);
+
+        if pops == 1 {
+            // Capture the garbage list.
+            let garbage = self.garbage.swap(ptr::null_mut(), Ordering::SeqCst);
+
+            if self.pops.fetch_sub(1, Ordering::SeqCst) != 1 {
+                let mut node = garbage;
+
+                while !node.is_null() {
+                    let next = (*node).next;
+                    let _ = Box::from_raw(node);
+                    node = next;
+                }
+            } else if !garbage.is_null() {
+                self.tie(garbage);
+            }
+
+            let _ = Box::from_raw(node);
+        } else {
+            self.tie(node);
+            self.pops.fetch_sub(1, Ordering::SeqCst);
+        }
+    }
+
+    unsafe fn tie(&self, list: *mut Node<T>) {
+        let mut last = list;
+
+        // Find the last item in the list.
+        while !(*last).next.is_null() {
+            last = (*last).next;
+        }
+
+        let mut head = self.garbage.load(Ordering::SeqCst);
+
+        loop {
+            (*last).next = head;
+
+            match self.garbage.compare_exchange_weak(head, list, Ordering::SeqCst, Ordering::SeqCst) {
+                Ok(_) => break,
+                Err(node) => head = node,
+            }
+        }
     }
 }
 
@@ -66,8 +135,8 @@ mod test {
 
     #[test]
     fn push_then_pop() {
-        const NUM_THREADS: usize = 1_000;
-        const NUM_PUSH_PER_THREAD: usize = 10_000;
+        const NUM_THREADS: usize = 10;
+        const NUM_PUSH_PER_THREAD: usize = 10;
 
         let stack = Stack::<usize>::new();
 
